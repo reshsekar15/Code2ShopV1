c7d310f9a51569704a0e3ecb1f0171d8700b1cd2
reshsekar15 <reshsekar15@gmail.com>
2018-07-10 09:14:06 -0700

    request form data accessed and added to database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import psycopg2
 from models import db
 from models import Challenges
@@ -18,32 +18,34 @@ def index():
 def challenges():
 	return render_template("challenges.html")
 	
-@app.route('/challengeupload', methods = ['POST', 'GET'])
-def challengeupload():
-	if request.method == 'POST':
-		challengeID = request.form('challengeid')
-		challengename = request.form('challengename')
-		challengepoints = request.form('challengepoints')
-		
-		#add to database
-		db.session.add(challengeid)
-		db.session.commit()		
+@app.route('/challengeupload', methods = ['GET', 'POST'])
+def challengeupload():	
 	return render_template("challengeupload.html")
 	
-@app.route('/challengeuploadsuccess', methods = ['POST', 'GET'])
+@app.route('/challengeuploadsuccess', methods = ['GET', 'POST'])
 def challengeuploadsuccess():
-	#if request.method == 'POST':
-		#challengeID = request.form('challengeid')
-		#challengename = request.form('challengename')
-		#challengepoints = request.form('challengepoints')
+	if request.method == 'POST':
+		valchallengeID = request.form['challengeid']
+		valchallengename = request.form['challengename']
+		valchallengepoints = request.form['challengepoints']
 		
 		#add to database
-	#session = Sessionmaker()
-	challenge_row = Challenges(challengeID = 1, challengeName = 'Test challenge', challengePoints = 100)
-	db.session.add(challenge_row)
-	db.session.commit()		
+		challenge_row = Challenges(challengeID = valchallengeID , challengeName =  valchallengename, challengePoints = valchallengepoints)
+		db.session.add(challenge_row)
+		db.session.commit()	
+		return render_template("challenges.html")
 	return render_template("challengeuploadsuccess.html")
-    
+	
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+	
 if __name__ == '__main__':
     app.debug = True
     app.run()
